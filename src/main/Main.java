de79934647c8f3ab672c307b011afc8aad735953
Bart Lyons <lyons.bart@protonmail.com>
2022-01-14 03:38:35 -0500

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load((getClass().getResource("/view/loginView.fxml")));
+        stage.setTitle("Scheduler v1.0");
+        stage.setScene(new Scene(root,500,300));
+        stage.show();
+    }
+}
