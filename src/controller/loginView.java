8fc88992c95abe6c5a4fa49f4dd2f5ff5020127a
Bart Lyons <lyons.bart@protonmail.com>
2022-01-14 03:56:31 -0500

    Added eventing handling for login button and fxid for the controls on the loginView.fxml.

---
+++ src/controller/loginView.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/** The controller that loginView uses. */
+public class loginView {
+
+    public TextField usernameTextField;
+    public Label errorMessageLabel;
+    public TextField passwordTextField;
+
+/** Event handler for login button.
+ * @param actionEvent */
+    public void loginButtonPressed(ActionEvent actionEvent) {
+        // event handling for login button.
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load((getClass().getResource("/view/loginView.fxml")));
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/view/loginView.fxml"))));
         stage.setTitle("Scheduler v1.0");
         stage.setScene(new Scene(root,500,300));
         stage.show();
