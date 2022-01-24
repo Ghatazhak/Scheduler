2bb580fb6fa7e6757d5a4edb11da4a111323ca47
Bart Lyons <lyons.bart@protonmail.com>
2022-01-23 21:07:16 -0500

    Contact Report working.

--- src/controller/ContactReportView.java
+++ src/controller/ContactReportView.java
@@ -1,8 +1,21 @@
 package controller;
 
+import data_access.AppointmentMSQL;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Appointment;
 
-public class ContactReportView {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ContactReportView implements Initializable {
+    ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
+    ObservableList<Appointment> appointmentsByContact = FXCollections.observableArrayList();
+    public TableView contactReportTableView;
     public TableColumn appointmentIDCol;
     public TableColumn titleCol;
     public TableColumn typeCol;
@@ -10,4 +23,24 @@ public class ContactReportView {
     public TableColumn startCol;
     public TableColumn endCol;
     public TableColumn customerIDCol;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        allAppointments = AppointmentMSQL.findAll();
+        for(Appointment a: allAppointments){
+            if(a.getContactId() == ContactSelectForReportView.tempContact.getContactId()){
+                appointmentsByContact.add(a);
+            }
+        }
+        contactReportTableView.setItems(appointmentsByContact);
+
+        appointmentIDCol.setCellValueFactory(new PropertyValueFactory<>("appointmentId"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+        startCol.setCellValueFactory(new PropertyValueFactory<>("startDateTime"));
+        endCol.setCellValueFactory(new PropertyValueFactory<>("endDateTime"));
+        customerIDCol.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+
+    }
 }
--- src/controller/ContactSelectForReportView.java
+++ src/controller/ContactSelectForReportView.java
@@ -1,7 +1,11 @@
 package controller;
 
+import data_access.ContactMYSQL;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
@@ -11,17 +15,30 @@ import model.Contact;
 import view.FXMLLoaderInterface;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ContactSelectForReportView {
+public class ContactSelectForReportView implements Initializable {
+    public static Contact tempContact;
+    private ObservableList<Contact> allContacts = FXCollections.observableArrayList();
     public ComboBox<Contact> contactCB;
+
 
     FXMLLoaderInterface loaderLambda = s -> {
         Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource(s))));
         return root;
     };
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        allContacts = ContactMYSQL.findAll();
+        contactCB.setItems(allContacts);
+    }
+
     public void generateButtonClicked(ActionEvent actionEvent) throws IOException {
+        tempContact = contactCB.getSelectionModel().getSelectedItem();
+
         Parent root = loaderLambda.getRoot("/view/ContactReportView.fxml");
         Stage stage = (Stage) contactCB.getScene().getWindow();
         Scene scene = new Scene(root, 782, 410);
@@ -38,4 +55,6 @@ public class ContactSelectForReportView {
         stage.setScene(scene);
         stage.show();
     }
+
+
 }
